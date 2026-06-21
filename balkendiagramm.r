f4a1fe8d846de5a9419a8e95ac611a43834d9283
ROOTFORCE <45320679+OkanAcikel@users.noreply.github.com>
2026-06-21 20:44:55 +0200

    Update barplot colors and data in balkendiagramm.r

--- balkendiagramm.r
+++ balkendiagramm.r
@@ -5,3 +5,12 @@ barplot(x,
         main = "Verkäufe pro Quartal",
         names.arg = c("Q1", "Q2", "Q3", "Q4"),
         col = "lightgreen")
+
+
+
+x <- c(12, 18, 9, 15)
+
+barplot(x,
+        main = "Verkäufe pro Quartal",
+        names.arg = c("Q1", "Q2", "Q3", "Q4"),
+        col = c("lightblue", "green", "red", "pink"))
